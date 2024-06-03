fb04af25c8f0a723f547b163bdc15d8cd0910c70
leguter <kolalevcenko80@gmail.com>
2024-06-02 17:19:07 -1200

    6

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,30 +6,29 @@ const inputEl = document.querySelector('.feedback-form');
 const btnSubmit = document.querySelector('.btn-submit');
 const areaEl = document.querySelector('.textarea-form');
 inputEl.addEventListener('input', (e) => {
-    formData.email = inputEl.elements.email.value;
-    formData.message = inputEl.elements.message.value;
+    formData.email = inputEl.elements.email.value.trim();
+    formData.message = inputEl.elements.message.value.trim();
     const zip = JSON.stringify(formData);
     localStorage.setItem('feedback-form-state', zip);
 });
-if (localStorage.getItem('feedback-form-state').length !== 0) {
-    const parseData = JSON.parse(localStorage.getItem('feedback-form-state'))
+const parseData = JSON.parse(localStorage.getItem('feedback-form-state'))
+if (parseData !== '' ) {
     inputEl.elements.email.value = parseData.email;
     inputEl.elements.message.value = parseData.message;
     formData.email = inputEl.elements.email.value;
     formData.message = inputEl.elements.message.value;
 };
-btnSubmit.addEventListener('submit', (e) => {
+inputEl.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (formData.email === '' ?? formData.message === '') {
-         alert('Fill please all fields');
+    if (formData.email === '' || formData.message === '') {
+        alert('Fill please all fields');
+        return;
     } else {
-        console.log(formData.email, formData.message);
-        localStorage.clear();
+        console.log(formData);
+
+    } 
+    inputEl.reset();
         formData.email = '';
         formData.message = '';
-        inputEl.value = '';
-        areaEl.value = '';
-
-    }
-    
+    localStorage.removeItem('feedback-form-state');
 });
